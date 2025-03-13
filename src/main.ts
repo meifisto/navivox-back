9e1a6697a52a60a7beb45e2c1aff8a2fe61f168f
meifisto <xenooow@gmail.com>
2025-03-13 15:44:10 +0100

    show router paths

--- src/main.ts
+++ src/main.ts
@@ -2,35 +2,51 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { existsSync, mkdirSync } from 'fs';
+import { Logger } from '@nestjs/common';
 import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log'], // Exclut 'debug' et 'verbose'
+    logger: ['error', 'warn', 'log'],
   });
+
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  // Vérifie et crée le dossier s'il n'existe pas
   const uploadDir = './files/store';
   if (!existsSync(uploadDir)) {
     mkdirSync(uploadDir, { recursive: true });
     console.log(`📂 Dossier créé : ${uploadDir}`);
   }
-  // ---------------------------------------------
 
-  // Configuration Swagger
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('Documentation de l’API')
     .setVersion('1.0')
-    .addBearerAuth() // Ajoute l'authentification JWT
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // Route Swagger: /api
-  // ---------------------------------------------
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(process.env.PORT || 9999, '0.0.0.0');
   console.log(`🚀 Server is running on port ${process.env.PORT || 9999}`);
+
+  const server = app.getHttpAdapter().getInstance();
+  const router = server.router;
+
+  const availableRoutes: [] = router.stack
+    .map((layer) => {
+      if (layer.route) {
+        return {
+          route: {
+            path: layer.route?.path,
+            method: layer.route?.stack[0].method,
+          },
+        };
+      }
+    })
+    .filter((item) => item !== undefined);
+  console.log(availableRoutes);
 }
+
 bootstrap();
