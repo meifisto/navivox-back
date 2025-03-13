6346a7ec66d0685d367310b4bf4055f572507fe0
meifisto <xenooow@gmail.com>
2025-03-13 15:02:24 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -31,5 +31,6 @@ async function bootstrap() {
   // ---------------------------------------------
 
   await app.listen(process.env.PORT || 9999, '0.0.0.0');
+  console.log(`🚀 Server is running on port ${process.env.PORT || 9999}`);
 }
 bootstrap();
