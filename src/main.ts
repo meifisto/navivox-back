055b7ee7af20e0ba690c44e329e5c2a23dfce519
meifisto <xenooow@gmail.com>
2025-04-23 18:17:03 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
 
-  await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(`🚀 Server is running on port ${process.env.PORT || 3000}`);
 
   // const server = app.getHttpAdapter().getInstance();
